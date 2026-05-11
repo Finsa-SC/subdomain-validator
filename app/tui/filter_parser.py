edead826a453e026c9dcd02d1bb808bc5bf83f7b
Finsa-SC <finsakusumaputra@gmail.com>
2026-05-11 19:37:29 +0700

    fix: fix logic error in filter and regex

--- app/tui/filter_parser.py
+++ app/tui/filter_parser.py
@@ -22,58 +22,87 @@ class FilterParser:
             return not self.matches(query[4:], result)
 
         if "status:" in query:
-            match = re.search(r'status:(\d+|forbidden|redirect)', query)
+            match = re.search(r'status:([\w,]+)', query)
             if match:
-                target = match.group(1)
+                targets = match.group(1).split(",")
                 h_status = result.get("http", {}).get("status")
                 s_status = result.get("https", {}).get("status")
 
                 forbidden = (401, 402, 403)
                 redirect = (301, 302, 307, 308)
-                if target == 'forbidden':
-                    if h_status not in forbidden and s_status not in forbidden:
-                        return False
-                elif target == 'redirect':
-                    if h_status not in redirect and s_status not in redirect:
-                        return False
-                else:
-                    target_code = int(target)
-                    if h_status != target_code and s_status != target_code:
-                        return False
+
+                matched = False
+                for target in targets:
+                    target = target.strip()
+                    if target == 'forbidden':
+                        if h_status in forbidden or s_status in forbidden:
+                            matched = True
+                    elif target == 'redirect':
+                        if h_status in redirect or s_status in redirect:
+                            matched = True
+                    else:
+                        try:
+                            target_code = int(target)
+                            if h_status == target_code or s_status == target_code:
+                                matched = True
+                        except ValueError:
+                            pass
+                if not matched:
+                    return False
 
         if 'server:' in query:
-            match = re.search(r'server:(\w+)', query)
+            match = re.search(r'server:([\w+,-]+)', query)
             if match:
-                target = match.group(1)
-                server = result.get("http", {}).get("server", "").lower()
-                if target not in server:
+                targets = [x.strip() for x in match.group(1).split(",")]
+                server = (
+                    result.get("http", {}).get("server", "").lower() + " " +
+                    result.get("https", {}).get("server", "").lower()
+                ).lower()
+
+                if not any(target in server for target in targets):
                     return False
 
         if 'honeypot:' in query:
-            match = re.search(r'honeypot:(\w+)', query)
+            match = re.search(r'honeypot:([\w+,]+)', query)
             if match:
-                target = match.group(1)
+                targets = [x.strip() for x in match.group(1).split(",")]
                 score = result.get("honeypot_score", 0)
                 label = result.get("honeypot_label", "Unlikely").lower()
-                if target == 'true':
-                    if score < 0.5:
-                        return False
-                if target == 'false':
-                    if score >= 0.5:
-                        return False
-                else:
-                    if label != target:
-                        return False
+                matched = False
+                for target in targets:
+                    if target == 'true':
+                        if score >= 0.5:
+                            matched = True
+                    elif target == 'false':
+                        if score < 0.5:
+                            matched = True
+                    elif label == target:
+                            matched = True
+
+                if not matched:
+                    return False
 
         if 'subdomain:' in query:
-            match = re.search(r'subdomain:(\S+)', query)
+            match = re.search(r'subdomain:([\w.*,-]+)', query)
             if match:
-                target = match.group(1)
-                if target not in result.get("subdomain", "").lower():
+                targets = [x.strip() for x in match.group(1).split(",")]
+                subdomain = result.get("subdomain", "")
+                matched = False
+
+                for target in targets:
+                    pattern = (
+                        target
+                        .replace(".", r"\.")
+                        .replace("*", ".*")
+                    )
+                    if re.search(pattern, subdomain):
+                        matched = True
+                        break
+                if not matched:
                     return False
 
         if 'ip:' in query:
-            match = re.search(r'ip:(\S+)', query)
+            match = re.search(r'ip:([\d.*,\w]+)', query)
             if match:
                 target = match.group(1)
                 ip_str = result.get("ip_address", "")
@@ -92,7 +121,7 @@ class FilterParser:
                     if not proxy:
                         return False
                 else:
-                    pattern = target.replace(".", r"\.").replace("*", r".")
+                    pattern = target.replace(".", r"\.").replace("*", ".*")
                     if not re.fullmatch(pattern, ip_str):
                         return False
 
