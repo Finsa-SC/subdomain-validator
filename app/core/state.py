f1bb202e72fb61205ae707ca8c4477c8c7a669b5
Finsa-SC <finsakusumaputra@gmail.com>
2026-05-15 10:06:18 +0700

    feat: add state for program running status

---
+++ app/core/state.py
@@ -0,0 +1,13 @@
+import threading
+
+class AppState:
+    def __init__(self):
+        self.is_running = True
+        self.data_store = {}
+        self._lock = threading.Lock()
+
+    def stop(self):
+        with self._lock:
+            self.is_running = False
+
+app_state = AppState()
