8367a3bcd522380e361c1c71cf679961d34974ff
Finsa-SC <finsakusumaputra@gmail.com>
2026-05-16 18:51:12 +0700

    refactor: refactor send request calling in hacker target source

--- app/sources/hackertarget.py
+++ app/sources/hackertarget.py
@@ -1,11 +1,15 @@
-import requests
 from utils import get_logger
 
 def fetch_hackertarget(domain: str):
+    from core import send_request
     log = get_logger("hackertarget")
     try:
         url = f"https://api.hackertarget.com/hostsearch/?q={domain}"
-        response = requests.get(url=url, timeout=5)
+        response = send_request(method="GET", url=url, timeout=5)
+
+        if not response:
+            log.error("Failed to fetch data from HackerTarget (Connection or Proxy Error).")
+            return
 
         if "error" in response.text.lower() or response.status_code != 200:
             log.error(f"Error: {response.text}")
@@ -14,7 +18,5 @@ def fetch_hackertarget(domain: str):
             sub = line.split(",")[0].lower().strip()
             if sub:
                 yield sub
-    except requests.exceptions.Timeout:
-        log.error("Request Timeout")
-    except requests.exceptions.RequestException as e:
-        log.error(f"{e}")
+    except Exception as e:
+        log.error(f"Unexpected error in hackertarget module: {e}")
