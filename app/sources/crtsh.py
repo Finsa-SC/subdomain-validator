a585c5195315501f68c850c41ebbe57ba533ed2e
Finsa-SC <finsakusumaputra@gmail.com>
2026-05-16 18:54:16 +0700

    refactor: refactor send request calling in crt.sh source

--- app/sources/crtsh.py
+++ app/sources/crtsh.py
@@ -1,11 +1,16 @@
-import requests
 import ijson
 from utils import get_logger
+
 def fetch_crtsh(domain: str):
+    from core import send_request
     log = get_logger("crtsh")
     url = f"https://crt.sh/?q={domain}&output=json"
     try:
-        res = requests.get(url, timeout=10)
+        res = send_request(method="GET", url=url, timeout=10)
+        if not res:
+            log.error("Failed to fetch data from crt.sh (Connection or Proxy Error).")
+            return
+
         if res.status_code == 200:
             for entry in ijson.items(res.raw, "item"):
                 name = entry['name_value'].lower()
@@ -13,7 +18,5 @@ def fetch_crtsh(domain: str):
                     clean = n.replace("*.", "").strip()
                     if clean:
                         yield clean
-    except requests.exceptions.Timeout:
-        log.error("Request Timeout")
-    except requests.exceptions.RequestException as e:
-        log.error(f"{e}")
+    except Exception as e:
+        log.error(f"Unexpected error in hackertarget module: {e}")
