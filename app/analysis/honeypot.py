f999ac4de681c91b44afd441139250861d8e8ff8
Finsa-SC <finsakusumaputra@gmail.com>
2026-05-21 08:24:25 +0700

    refactor(honeypot): move honeypot list/signature signatures.py --[move]-> honeypot.py

--- app/analysis/honeypot.py
+++ app/analysis/honeypot.py
@@ -1,8 +1,118 @@
-from models import (HONEYPOT_TITLE, HONEYPOT_NAME, HONEYPOT_HASHES, HONEYPOT_SERVERS, OBSOLETE_VERSIONS,
-    SUSPICIOUS_HEADER_ORDERS, SIGNAL_TIER, SIGNAL_WEIGHTS, CONFIDENCE_LABELS, HONEYPOT_HEADERS, KNOWN_PROXY_SERVERS)
-
 import math
 
+
+HONEYPOT_NAME = [
+    "admin", "login", "portal", "dashboard",
+    "setup", "db", "internal", "vpn", "staging",
+    "dev", "test", "backup", "secret", "hidden",
+    "shell", "cmd", "root", "secure",
+]
+
+HONEYPOT_TITLE = [
+    "iis7",
+    "iis windows server",
+    "apache2 ubuntu default page",
+    "apache2 debian default page",
+    "test page for apache",
+    "welcome to nginx",
+    "it works!",
+    "default web site page",
+    "index of /",
+    "directory listing",
+]
+
+HONEYPOT_HASHES = {
+    "5f352330a108a73b75a1334c264284d3": "Glastopf default login page",
+    "7215ee9c7d9dc229d2921a40e899ec5f": "Dionaea bare HTTP response",
+    "cfcd208495d565ef66e7dff9f98764da": "Heralding default response",
+    "b026324c6904b2a9cb4b88d6d61c81d1": "Conpot default ICS interface",
+    "d751713988987e9331980363e24189ce": "Python SimpleHTTPServer directory listing",
+}
+
+HONEYPOT_HEADERS = [
+    "x-honeypot",
+    "x-honey",
+    "x-deception",
+    "x-trap",
+    "x-canary",
+]
+
+HONEYPOT_SIZE_RANGE = [
+    "tiny_trap",
+    "login_trap",
+    "cms_trap"
+]
+
+
+HONEYPOT_SERVERS = [
+    # Web honeypots
+    "glastopf",
+    "wordpot",
+    "shockpot",
+    "dionaea",
+    "conpot",
+    "heralding",
+    "elasticpot",
+    "hellpot",
+    "basehttp",
+    "twistedweb",
+    "honeyd",
+    "simplehttp",
+    "scada",
+]
+OBSOLETE_VERSIONS = ["apache/2.2", "php/5.", "iis/7.0", "iis/6.0"]
+
+SUSPICIOUS_HEADER_ORDERS = [
+    ["server", "x-powered-by", "x-honeypot"],
+    ["x-aspnet-version", "x-powered-by", "server", "x-honeypot"],
+]
+
+
+SIGNAL_WEIGHTS = {
+    "hash_match":        0.95,
+    "honeypot_header":   0.92,
+    "server_sig_match":  0.82,
+    "obsolete_version":  0.65,
+    "identical_body_both_proto": 0.50,
+    "header_order":      0.52,
+    "clickbait_title":   0.48,
+    "subdomain_name":    0.20,
+    "missing_title":     0.14,
+}
+
+SIGNAL_TIER = {
+    "hash_match":       "critical",
+    "honeypot_header":  "critical",
+    "server_sig_match": "critical",
+    "obsolete_version": "strong",
+    "header_order":     "strong",
+    "clickbait_title":  "strong",
+    "identical_body_both_proto": "strong",
+    "subdomain_name":   "weak",
+    "missing_title":    "weak",
+}
+
+CONFIDENCE_LABELS = [
+    (0.90, "Confirmed"),
+    (0.75, "Likely"),
+    (0.50, "Probable"),
+    (0.25, "Possible"),
+    (0.00, "Unlikely"),
+]
+
+KNOWN_PROXY_SERVERS = [
+    "cloudflare",
+    "cloudfront",
+    "akamai",
+    "fastly",
+    "sucuri",
+    "imperva",
+    "varnish",
+    "edgecast",
+    "stackpath",
+    "bunnycdn",
+    "cdn77",
+]
 
 def noisy_or(probabilities: list[float]) -> float:
     if not probabilities:
--- app/models/signatures.py
+++ app/models/signatures.py
@@ -8,72 +8,6 @@ TITLE_IGNORE = [
     "404 not found",
     "used cloudflare to restrict access"]
 
-HONEYPOT_NAME = [
-    "admin", "login", "portal", "dashboard",
-    "setup", "db", "internal", "vpn", "staging",
-    "dev", "test", "backup", "secret", "hidden",
-    "shell", "cmd", "root", "secure",
-]
-
-HONEYPOT_TITLE = [
-    "iis7",
-    "iis windows server",
-    "apache2 ubuntu default page",
-    "apache2 debian default page",
-    "test page for apache",
-    "welcome to nginx",
-    "it works!",
-    "default web site page",
-    "index of /",
-    "directory listing",
-]
-
-HONEYPOT_HASHES = {
-    "5f352330a108a73b75a1334c264284d3": "Glastopf default login page",
-    "7215ee9c7d9dc229d2921a40e899ec5f": "Dionaea bare HTTP response",
-    "cfcd208495d565ef66e7dff9f98764da": "Heralding default response",
-    "b026324c6904b2a9cb4b88d6d61c81d1": "Conpot default ICS interface",
-    "d751713988987e9331980363e24189ce": "Python SimpleHTTPServer directory listing",
-}
-
-HONEYPOT_HEADERS = [
-    "x-honeypot",
-    "x-honey",
-    "x-deception",
-    "x-trap",
-    "x-canary",
-]
-
-HONEYPOT_SIZE_RANGE = [
-    "tiny_trap",
-    "login_trap",
-    "cms_trap"
-]
-
-
-HONEYPOT_SERVERS = [
-    # Web honeypots
-    "glastopf",
-    "wordpot",
-    "shockpot",
-    "dionaea",
-    "conpot",
-    "heralding",
-    "elasticpot",
-    "hellpot",
-    "basehttp",
-    "twistedweb",
-    "honeyd",
-    "simplehttp",
-    "scada",
-]
-OBSOLETE_VERSIONS = ["apache/2.2", "php/5.", "iis/7.0", "iis/6.0"]
-
-SUSPICIOUS_HEADER_ORDERS = [
-    ["server", "x-powered-by", "x-honeypot"],
-    ["x-aspnet-version", "x-powered-by", "server", "x-honeypot"],
-]
-
 PROXY_IPS = [
     # Cloudflare
     "173.245.48.0/20", "103.21.244.0/22", "103.22.200.0/22", "103.31.4.0/22", "141.101.64.0/18", "108.162.192.0/18",
@@ -146,52 +80,6 @@ PROXY_IPS = [
 
     # Leaseweb CDN
     "5.79.0.0/16",
-]
-
-SIGNAL_WEIGHTS = {
-    "hash_match":        0.95,
-    "honeypot_header":   0.92,
-    "server_sig_match":  0.82,
-    "obsolete_version":  0.65,
-    "identical_body_both_proto": 0.50,
-    "header_order":      0.52,
-    "clickbait_title":   0.48,
-    "subdomain_name":    0.20,
-    "missing_title":     0.14,
-}
-
-SIGNAL_TIER = {
-    "hash_match":       "critical",
-    "honeypot_header":  "critical",
-    "server_sig_match": "critical",
-    "obsolete_version": "strong",
-    "header_order":     "strong",
-    "clickbait_title":  "strong",
-    "identical_body_both_proto": "strong",
-    "subdomain_name":   "weak",
-    "missing_title":    "weak",
-}
-
-CONFIDENCE_LABELS = [
-    (0.90, "Confirmed"),
-    (0.75, "Likely"),
-    (0.50, "Probable"),
-    (0.25, "Possible"),
-    (0.00, "Unlikely"),
-]
-
-KNOWN_PROXY_SERVERS = [
-    "cloudflare",
-    "cloudfront",
-    "akamai",
-    "fastly",
-    "sucuri",
-    "imperva",
-    "varnish",
-    "edgecast",
-    "stackpath",
-    "bunnycdn",
-    "cdn77",
 ]
 
 USER_AGENT_FALLBACK = [
